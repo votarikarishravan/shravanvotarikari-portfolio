b26a5dcff3e30e39ac6c92e5f1e38c6b9c51182a
votarikarishravan <57553595+votarikarishravan@users.noreply.github.com>
2025-06-12 18:00:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/votarikarishravan-portfolio/' : '/',
+  base: mode === 'production' ? '/shravanvotarikari-portfolio/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
