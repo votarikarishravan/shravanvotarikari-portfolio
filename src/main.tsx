02a8fc0e3c8e87add93760e157da3adcf9ebc03f
shravanvotarikari <shravan.votarikari@eidiko-india.com>
2025-06-12 18:21:20 +0530

    done

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/votarikarishravan-portfolio">
+  <BrowserRouter basename="/shravanvotarikari-portfolio">
     <App />
   </BrowserRouter>
 );
