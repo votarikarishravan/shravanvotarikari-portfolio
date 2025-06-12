8ad445d37d3d55153564d37916539d7a2fc52aa5
shravanvotarikari <shravan.votarikari@eidiko-india.com>
2025-06-12 10:56:37 +0530

    updated code

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const plugins = [react()];
+
+  if (mode === "development") {
+    plugins.push(componentTagger());
+  }
+
+  return {
+    base: "/votarikarishravan-portfolio/", // Important for GitHub Pages
+    server: {
+      host: "::",
+      port: 8080,
+    },
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
