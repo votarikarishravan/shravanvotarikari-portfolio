f2a55b25bea0521fbdc68629f9f4f4ad95014c55
votarikarishravan <57553595+votarikarishravan@users.noreply.github.com>
2025-06-12 17:50:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const plugins = [react()];
 
@@ -12,7 +6,9 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    base: "/votarikarishravan-portfolio/", // Important for GitHub Pages
+    base: mode === "production" && process.env.DEPLOY_TARGET === "GH_PAGES"
+      ? "/votarikarishravan-portfolio/"
+      : "/", // ✅ set to "/" for Vercel
     server: {
       host: "::",
       port: 8080,
