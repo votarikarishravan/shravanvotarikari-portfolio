d605d74dbfd7bf70314de88e1bfe3cbdb867d4fa
votarikarishravan <57553595+votarikarishravan@users.noreply.github.com>
2025-06-12 18:24:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/shravanvotarikari-portfolio/' : '/',
+  base: process.env.DEPLOY_TARGET === 'GH_PAGES'
+    ? '/shravanvotarikari-portfolio/'  // For GitHub Pages
+    : '/',                              // For Vercel or local dev
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
   },
 }));
